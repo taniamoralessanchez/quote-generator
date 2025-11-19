e692d6156f5d92d792467ddc2df7aa4e6651a381
Martin <labradorgonzalezmartin@gmail.com>
2025-11-19 13:16:21 +0100

    feat: Estructurar citas con texto y autor explícitos

--- quotes.js
+++ quotes.js
@@ -1,14 +1,12 @@
 const quotes = [
-"La vida es lo que pasa mientras estás ocupado haciendo otros
-planes. - John Lennon",
-"El único modo de hacer un gran trabajo es amar lo que haces.
-- Steve Jobs",
-"La innovación distingue a un líder de un seguidor. - Steve
-Jobs"
+  { text: "La vida es lo que pasa mientras estás ocupado haciendo otros planes.", author: "John Lennon" },
+  { text: "El único modo de hacer un gran trabajo es amar lo que haces.", author: "Steve Jobs" },
+  { text: "La innovación distingue a un líder de un seguidor.", author: "Steve Jobs" }
 ];
+
 function getRandomQuote() {
-const randomIndex = Math.floor(Math.random() *
-quotes.length);
-return quotes[randomIndex];
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return `${quotes[randomIndex].text} - ${quotes[randomIndex].author}`;
 }
+
 export { quotes, getRandomQuote };

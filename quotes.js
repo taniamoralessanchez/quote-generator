246b81a81f16a6bb5abc284458a60cf8e11738e6
Martin <labradorgonzalezmartin@gmail.com>
2025-11-26 14:10:36 +0100

    feat: Añadir nueva cita de Aristóteles

--- quotes.js
+++ quotes.js
@@ -4,6 +4,8 @@ const quotes = [
   { text: "La innovación distingue a un líder de un seguidor.", author: "Steve Jobs" }
   { text: "El éxito no es la clave de la felicidad. La felicidad es la clave del éxito.", author: "Albert Schweitzer" },
   { text: "Tu tiempo es limitado, no lo desperdicies viviendo la vida de otra persona.", author: "Steve Jobs" }
+  { text: "Solo hay una forma de evitar las críticas: no hagas nada, no digas nada, y no seas nada.", author: "Aristóteles" }
+
 ];
 
 function getRandomQuote() {

502c36bda24922cd4b559ae1e2f37129eedc2185
Martin <labradorgonzalezmartin@gmail.com>
2025-11-26 14:09:20 +0100

    feat: Añadir nueva cita de Albert Schweitzer

--- quotes.js
+++ quotes.js
@@ -2,6 +2,8 @@ const quotes = [
   { text: "La vida es lo que pasa mientras estás ocupado haciendo otros planes.", author: "John Lennon" },
   { text: "El único modo de hacer un gran trabajo es amar lo que haces.", author: "Steve Jobs" },
   { text: "La innovación distingue a un líder de un seguidor.", author: "Steve Jobs" }
+  { text: "El éxito no es la clave de la felicidad. La felicidad es la clave del éxito.", author: "Albert Schweitzer" },
+  { text: "Tu tiempo es limitado, no lo desperdicies viviendo la vida de otra persona.", author: "Steve Jobs" }
 ];
 
 function getRandomQuote() {
